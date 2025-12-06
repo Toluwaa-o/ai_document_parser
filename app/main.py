f1800fe8d45b874c220d2824e9e03852d5b62b7e
Toluwalashe <asheogunleye02@gmail.com>
2025-12-06 04:17:41 +0100

    chore: dockerfile deploy command fix

--- app/main.py
+++ app/main.py
@@ -23,5 +23,5 @@ app.include_router(health_router)
 app.include_router(api_router, prefix="/api/v1")
 
 if __name__ == "__main__":
-    port = int(os.getenv.get("PORT", 8000))
+    port = int(os.getenv("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
